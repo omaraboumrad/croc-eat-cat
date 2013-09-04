20695691be9c1d7f80a6527bbe81fcc6c2f442a2
Omar Abou Mrad <xterm@wind>
2013-09-04 12:29:19 +0300

    new

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pygame
 pygame.init()
 
 def show_score(font, score):
-    return font.render('Score: {}'.format(score), 1, (255,255,0))
+    return font.render('Score: {}'.format(score), 1, (255,0,0))
 
-def generate_cat():
+def generate_point():
     return (random.randint(50,590), random.randint(50,430))
 
 
@@ -15,13 +15,14 @@ def main():
     window = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Batata')
     screen = pygame.display.get_surface()
+    clock = pygame.time.Clock()
 
     x, y = 50, 50
     score = 0
 
     croc_img = pygame.image.load('croc.png')
     cat_img = pygame.image.load('cat.png')
-    meow = pygame.mixer.Sound('meow.wav')
+    tiger_img = pygame.image.load('tiger.png')
 
     mono = pygame.font.SysFont('monospace', 15)
 
@@ -42,7 +43,8 @@ def main():
 
     choice = croc_map[0]
 
-    cats = map(lambda _: generate_cat(), range(10))
+    cats = map(lambda _: generate_point(), range(10))
+    tigers = []
 
     while is_running:
         ev = pygame.event.poll()
@@ -51,28 +53,30 @@ def main():
             if key == 27:
                 is_running = False 
 
-            if key == 274:
-                choice = random.choice(croc_map[:3])
-                y += 5
+        keys = pygame.key.get_pressed()
 
-            if key == 273:
-                choice = random.choice(croc_map[3:6])
-                y -= 5
+        if keys[pygame.K_DOWN]:
+            choice = random.choice(croc_map[:3])
+            y += 5
 
-            if key == 275:
-                choice = random.choice(croc_map[6:9])
-                x += 5
+        if keys[pygame.K_UP]:
+            choice = random.choice(croc_map[3:6])
+            y -= 5
 
-            if key == 276:
-                choice = random.choice(croc_map[9:12])
-                x -= 5
+        if keys[pygame.K_RIGHT]:
+            choice = random.choice(croc_map[6:9])
+            x += 5
+
+        if keys[pygame.K_LEFT]:
+            choice = random.choice(croc_map[9:12])
+            x -= 5
 
         if x > 600: x = 20
         if y > 440: y = 20
         if x < 20: x = 600
         if y < 20: y = 440
 
-        screen.fill((200,200,200))
+        screen.fill((255,255,255))
 
         for cat in cats:
             a = pygame.Rect(cat, cat_img.get_size())
@@ -81,15 +85,28 @@ def main():
             if b.colliderect(a):
                 score += 1
                 cats.remove(cat)
+                if score % 5 == 0: 
+                    tigers.append(generate_point())
             else:
                 screen.blit(cat_img, cat)
 
+        for tiger in tigers:
+            a = pygame.Rect(tiger, tiger_img.get_size())
+            b = pygame.Rect((x,y), choice[2:])
+
+            if b.colliderect(a):
+                score -= 5
+                tigers.remove(tiger)
+            else:
+                screen.blit(tiger_img, tiger)
+
         if len(cats) < 10:
-            cats.append(generate_cat())
+            cats.append(generate_point())
 
         screen.blit(croc_img, (x,y), choice)
         screen.blit(show_score(mono, score), (10,10))
         pygame.display.flip()
+        clock.tick(30)
 
 if __name__ == '__main__':
     main()
